1d45127560b5440da4612d54b5e14b6e703ec6d7
rbisha50 <rbishal50@gmail.com>
2020-07-23 16:50:06 +0545

    Filter list items

--- app.js
+++ app.js
@@ -1,55 +1,58 @@
 const itemsList = [
-    {
-        id: 1,
-        name: 'Bishal'
-    },
-    {
-        id: 2,
-        name: 'Ram'
-    },
-    {
-        id: 3,
-        name: 'Ekata'
-    },
-    {
-        id:4,
-        name: 'Bottle'
-    },
-    {
-        id:5,
-        name: 'Plant'
-    }
-  ]
+  {
+    id: 1,
+    name: 'Bishal',
+  },
+  {
+    id: 2,
+    name: 'Ram',
+  },
+  {
+    id: 3,
+    name: 'Ekata',
+  },
+  {
+    id: 4,
+    name: 'Bottle',
+  },
+  {
+    id: 5,
+    name: 'Plant',
+  },
+];
 
 function displayNames() {
-    for(let i = 0; i < itemsList.length; i++) {
-        document.getElementById("itemNames").innerHTML += itemsList[i].name + "<br />";  
+  const userInput = document.getElementById('txtField').value.toLowerCase();
+
+  const filtered = itemsList.filter((item) =>
+    item.name.toLowerCase().includes(userInput)
+  );
+
+  console.log(userInput, 'userInput');
+  console.log(filtered, 'filtered');
+
+  const dataToMap = userInput ? filtered : itemsList;
+
+  console.log(dataToMap, 'dataToMap');
+
+  if (userInput) {
+    document.getElementById('searchTitle').innerHTML = 'Search Results: ';
+    for (let i = 0; i < dataToMap.length; i++) {
+      document.getElementById('searchResults').innerHTML +=
+        dataToMap[i].name + '<br />';
     }
-}
-
-function searchItem() {
-    var userInput = document.getElementById("txtField").value;
-    document.getElementById("checkInput").innerHTML = userInput;
-
-
-    for(let j = 0; j < itemsList.length; j++) {
-        if (userInput == itemsList[j].name) {
-            document.getElementById("exists").innerHTML = "YES";
-        } else {
-            document.getElementById("exists").innerHTML = "asdfas";
-        }
+  } else {
+    for (let i = 0; i < dataToMap.length; i++) {
+      document.getElementById('itemNames').innerHTML +=
+        dataToMap[i].name + '<br />';
     }
-    
-
-
+  }
 }
-
 
 displayNames();
 
-
-submitBtn.addEventListener('click', searchItem);
+document.getElementById('submitBtn').addEventListener('click', displayNames);
 
 //   step 1 store the user input text in a variable
-//   step 2 check if the letters of the variable matches with the elements in array 
+//   step 2 check if the letters of the variable matches with the elements in array
 //   step 3 display the element
